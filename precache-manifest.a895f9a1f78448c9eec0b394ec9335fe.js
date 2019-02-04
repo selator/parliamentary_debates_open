8634934920d1bdd1200cfa6da006b598869634c7
Daniel Kantor <git@daniel-kantor.com>
2019-02-04 15:23:12 +0100

    Updates

---
+++ precache-manifest.a895f9a1f78448c9eec0b394ec9335fe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "66e701361090f3f1581d",
+    "url": "/parliamentary_debates_open/static/css/main.87063bed.chunk.css"
+  },
+  {
+    "revision": "66e701361090f3f1581d",
+    "url": "/parliamentary_debates_open/static/js/main.66e70136.chunk.js"
+  },
+  {
+    "revision": "9cd0fe86799f862fcf0a",
+    "url": "/parliamentary_debates_open/static/css/1.b5268fcf.chunk.css"
+  },
+  {
+    "revision": "9cd0fe86799f862fcf0a",
+    "url": "/parliamentary_debates_open/static/js/1.9cd0fe86.chunk.js"
+  },
+  {
+    "revision": "4aef6686173dcca2fc35",
+    "url": "/parliamentary_debates_open/static/js/runtime~main.4aef6686.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/parliamentary_debates_open/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/parliamentary_debates_open/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/parliamentary_debates_open/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/parliamentary_debates_open/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/parliamentary_debates_open/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "d05b523342c8e703032568bb43c0e974",
+    "url": "/parliamentary_debates_open/index.html"
+  }
+];
